27a438e6e701f158f4122aa7626cf62e6f3572d9
Oliver Guggenbühl <o.guggenbuehl@gmail.com>
2019-11-03 15:08:35 +0100

    Update btest.py

--- btest.py
+++ btest.py
@@ -2,6 +2,8 @@ import base64
 
 data = "abc123!?$*&()'-=@~"
 
+data2 = "bWFjaGluZV9yb2xle3JvbGU9IndvcmtlciJ9IDEK"
+
 def encodeB64(data):
   # URL and Filename Safe Base64 Encoding
   urlSafeEncodedBytes = base64.urlsafe_b64encode(data.encode("utf-8"))
@@ -19,6 +21,9 @@ def dump():
                 'source': 'data:text/plain;charset=utf-8;base64,{}'.format(encodeB64(data))
             }}
 
-#print(encodeB64(data))
+#ignition
+https://github.com/utilitywarehouse/on-prem-kube/blob/master/ignition/ic/worker0.ign
+
+print(decodeB64(data2))
 print(dump())
 #print(decodeB64(encodeB64(data)))
